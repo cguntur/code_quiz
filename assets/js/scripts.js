9848d7555cbb05874ad8393693f2d70ec1b37c9a
Chandrika Sista <chandrikasista@Chandrikas-MBP.attlocal.net>
2024-03-04 19:22:36 -0800

    Added the page to show the high scores. Modifed the code to show the next question without a button click and code to save the data to local variables

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,6 +19,12 @@ var timerEl = document.querySelector(".timer_count");
 var questions = document.querySelectorAll(".question");
 var answerValue = document.querySelector("#answer_value");
 var scoreEl = document.querySelector(".score_count");
+var finalPage = document.querySelector(".final_page");
+var questionsEl = document.querySelector(".questions_container");
+var initalsInput = document.querySelector(".initials");
+var scoresEl = document.querySelector(".show_scores");
+var displayScoresEl = document.querySelector(".display_score");
+var highScoresEl = document.querySelector("#high_scores");
 
 //get the button elements
 var nextQuestionButton = document.querySelector("#next_question");
@@ -31,26 +37,37 @@ var submitButton = document.querySelector("#submit");
 var initials = localStorage.getItem("initials");
 var score = 0;
 let currentQuestionIndex = 0;
+var timer = "";
+var studentGrades = JSON.parse(localStorage.getItem('studentGrades')) || [];
+var saved_scores = JSON.parse(localStorage.getItem("studentGrades"));
+
+if(saved_scores){
+    var scores_output = "";
+    Object.entries(saved_scores).forEach(([key, value]) => {
+        scores_output += "<p>User: " + value.initials + " Score: " + value.score + "</p>";
+    });
+    if(highScoresEl){
+        highScoresEl.innerHTML = scores_output;
+    }
+}
 
 //Click Events
-startButton.addEventListener("click", startQuiz);
-nextQuestionButton.addEventListener("click", showQuestion);
-submitButton.addEventListener("click", submitAnswer);
-//answerRadioBtn.forEach(function(radioButton) {
-//    var isCorrectAnswer = radioButton.dataset.correct;
-//    //console.log("Is Correct Answer: " + isCorrectAnswer);
-//});
+if(startButton){
+    startButton.addEventListener("click", startQuiz);
+}
+
 
 //Call the checkAnswer function when ever the radio button is clicked
-for(i=0; i<answerRadioBtn.length; i++){
-    answerRadioBtn[i].addEventListener("click", checkAnswer);
+if(answerRadioBtn){
+    for(i=0; i<answerRadioBtn.length; i++){
+        answerRadioBtn[i].addEventListener("click", processQuestion);
+    }
 }
-//console.log(answerRadioBtn);
 
 //Start quiz
 function startQuiz(){
     //Set the timer count
-    timerCount = 50;
+    timerCount = 30;
     
     //Call the start timer function
     startTimer();
@@ -66,34 +83,41 @@ function startTimer(){
         timerEl.textContent = "Time Left: " + timerCount;
         if(timerCount === 0){
             clearInterval(timer);
+            timerEl.textContent = "Time Left: " + timerCount + ". Time is up!!";
+
+            // loop through the array
+            // using forEach
+            answerRadioBtn.forEach(function (button) {
+                button.disabled = true;
+            });
         }
     }, 1000);
 }
 
 function showQuestion(){
     answerValue.textContent = "";
-    var hideQuestion = questions[currentQuestionIndex -1];
-    var questionToShow = questions[currentQuestionIndex];
 
-    //If the user goes to the next question, hide the previous question and show the next question
-    if(currentQuestionIndex > 0){
-        hideQuestion.classList.add("hide");
+    if(currentQuestionIndex < questions.length){
+        var hideQuestion = questions[currentQuestionIndex -1];
+        var questionToShow = questions[currentQuestionIndex];
+
+        //If the user goes to the next question, hide the previous question and show the next question
+        if(currentQuestionIndex > 0){
+            hideQuestion.classList.add("hide");
+        }
+        if(questions[currentQuestionIndex] != questions.length){
+            questionToShow.classList.remove("hide");
+        }
+        //Hide the start button when the question is displayed
+        hideStartButton();
     }
-    questionToShow.classList.remove("hide");
-
-    //Hide the start button when the question is displayed
-    hideStartButton();
-
-    //Show the next button when the question is displayed
-    showNextButton();
-
     //If the user is on the last question, hide the next button and show the submit button
-    if(currentQuestionIndex == questions.length -1){
-        hideNextButton();
-        showSubmitButton();
+    if(currentQuestionIndex == questions.length){
+        console.log("Show final page.");
+        showFinalPage();
     }
     currentQuestionIndex++;
-}    
+}
 
 /**
  * Functions to hide and show the appropriate buttons
@@ -103,22 +127,19 @@ function hideStartButton(){
     startButton.classList.add("hide");
 }
 
-function showNextButton(){
-    nextQuestionButton.classList.remove('hide');
+function processQuestion(){
+    //Check the answer
+    console.log(this);
+    checkAnswer(this);
+
+    //Show the next question
+    showNextQuestion();
 }
 
-function showSubmitButton(){
-    submitButton.classList.remove("hide");
-}
-
-function hideNextButton(){
-    nextQuestionButton.classList.add('hide');
-}
-
-
-function checkAnswer(){
+function checkAnswer(question){
+    console.log(question);
     //get the dataset attached to the radio buttons. This tells us whether the answer is correct or not    
-    var isCorrectAnswer = this.dataset.correct;
+    var isCorrectAnswer = question.dataset.correct;
 
     //If the answer is correct/incorrect, update the score and dispaly that on the screen
     if(isCorrectAnswer === "yes"){
@@ -129,6 +150,43 @@ function checkAnswer(){
         answerValue.textContent = "InCorrect Answer!";
         timerCount -= 5;
     }
+}
+
+function showNextQuestion(){
+    //Show the next question
+    setTimeout(() => {
+        console.log("Delayed for 1 seconds.");
+        showQuestion();
+      }, 1000);
+}
+
+function showFinalPage(){
+    stopTimer();
+    questionsEl.classList.add("hide");
+    finalPage.classList.remove("hide");
+
+    submitButton.addEventListener("click", submitScores);
+}
+function submitScores(event){
+    console.log("Submit Scores");
+    var user_initials = initalsInput.value;
+    console.log("User Initials: " + user_initials);
+    // Prevent default action
+    event.preventDefault();
+    //console.log(event);
+    var studentGrade = {
+        initials: user_initials,
+        score: score,
+    };
+    studentGrades.push(studentGrade);
+
+    console.log(JSON.stringify(studentGrades))
+    
+    localStorage.setItem("studentGrades", JSON.stringify(studentGrades));
+    finalPage.classList.add("hide");
+    submitButton.classList.add("hide");
+    scoresEl.classList.remove("hide");
+    displayScoresEl.textContent = "User: " + initials + " " + "Score: " + score;
 }
 
 //Function to update the scores
@@ -142,7 +200,16 @@ function updateScore(isCorrectAnswer){
 }
 
 function submitAnswer(){
+    stopTimer();
     initials = prompt("Please enter your initials.");
-    localStorage.setItem("initials", initials);
-    localStorage.setItem("score", score);
+    var studentGrade = {
+        initials: initials,
+        score: score,
+    };
+}
+
+function stopTimer(){
+    timerCount = 0;
+    clearInterval(timer);
+    timerEl.textContent = "Time Left: " + timerCount + ". You finished the quiz.";
 }
